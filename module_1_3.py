f5f6798d8a09e0ee82d4deaaa28bafffc57a69cb
SergeyMikhin <akvoliahim@gmail.com>
2024-10-03 21:13:38 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,13 @@
+name = "Sergey"
+print(name, type(name))
+age = 24
+print(age, type(age))
+new_age = 25
+print(new_age, type(new_age))
+print(age + new_age)
+is_student = "True"
+print(is_student, type(is_student))
+print("Name: Sergey")
+print('Age: 24')
+print("New_age: 25")
+print("Is Student: True")
